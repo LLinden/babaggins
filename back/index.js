77b08b0339858e0fd49ce3bd73d3c8a6ac8f863c
Tales-K <talesrk1@gmail.com>
2025-06-07 17:50:49 -0300

    fix: remove cors restriction from backend

--- back/index.js
+++ back/index.js
@@ -3,11 +3,7 @@ const cors = require("cors");
 const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
-app.use(
-  cors({
-    origin: "https://babaggins-web.onrender.com",
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 // Conecta ao banco (cria o arquivo se não existir)
